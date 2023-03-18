bd377bc52c4e525a4cfd401b62bc26bc25893430
yyyyyao <yyyyyao@htomail.co.jp>
2023-03-19 01:48:20 +0900

    Debugボタンの追加。

--- RFViewerHome.py
+++ RFViewerHome.py
@@ -414,6 +414,11 @@ class RFViewerHomeWidget(RFViewerWidget):
         AllResetButton.setIcon(Icons.AllReset)
         self._toolbarWidget.addButton(AllResetButton)
 
+        # for Debug.
+        DebuggerAttachButton = createButton("", self.DebuggerAttach)
+        DebuggerAttachButton.setIcon(Icons.rightarrow)
+        self._toolbarWidget.addButton(DebuggerAttachButton)
+
     def _configureToolbarAnnotationSection(self):
         self._toolbarWidget.createSection(self.tr("Measuring Instruments"))
         # self._toolbarWidget.addButton(createButton(self.tr("Measuring Instruments"), self.loadAnnotationModule))
@@ -532,6 +537,29 @@ class RFViewerHomeWidget(RFViewerWidget):
                     slice.SetWidgetVisible(False)
                 self._currentWidget1 = slicer.modules.RFVisualizationWidget
                 self._currentWidget1.ui.setMIPThickness5Button()
+
+    def DebuggerAttach(self):
+        msg = qt.QMessageBox()
+        msg.setIcon(qt.QMessageBox.Information)
+
+        msg.setText("DebuggerのAttachをwaitします。")
+        msg.setInformativeText("よろしいですか？")
+
+        pButtonYes = msg.addButton("はい", qt.QMessageBox.YesRole)
+        msg.addButton("キャンセル", qt.QMessageBox.NoRole)
+        msg.exec_()
+
+        if msg.clickedButton() == pButtonYes:
+            import ptvsd
+            ptvsd.enable_attach()
+            ptvsd.wait_for_attach()
+
+            msgAttached = qt.QMessageBox()
+            msgAttached.setIcon(qt.QMessageBox.Information)
+            msgAttached.setText("Debugger Attached!!")
+            msg.addButton("OK", qt.QMessageBox.NoRole)
+            msgAttached.exec_()
+
     def loadVisualisationModule(self):
         self._currentWidget = slicer.modules.RFVisualizationWidget
         self._moduleWidget.setModule(self._visualizationWidget, self.tr("Visualization"))
